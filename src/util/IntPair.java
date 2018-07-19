2e1e2876faa4cd0b00fb55bd219507606715890e
Ethan Halliday <ethanhalliday@cmail.carleton.ca>
2018-07-18 20:14:04 -0400

    Add files via upload

---
+++ src/util/IntPair.java
@@ -0,0 +1,15 @@
+package util;
+
+public class IntPair {
+    private int x;
+    private int y;
+    public IntPair(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+    public int getX(){ return x; }
+    public int getY(){ return y; }
+
+    public void setX(int _x){ x = _x; }
+    public void setY(int _y){ y = _y; }
+}
